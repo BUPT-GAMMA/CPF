ace91b3ce8ff40815658c33b9bae8b4ab332a274
Jiawei <Liu_Jiawei@bupt.edu.cn>
2021-07-09 17:36:38 +0800

    Add files via upload

---
+++ data/get_citation.py
@@ -0,0 +1,124 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.nn.functional as F
+import sys
+import pickle as pkl
+import networkx as nx
+import json
+from networkx.readwrite import json_graph
+import pdb
+sys.setrecursionlimit(99999)
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
+
+def normalize(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    rowsum = (rowsum == 0) * 1 + rowsum
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def sys_normalized_adjacency(adj):
+    adj = sp.coo_matrix(adj)
+    adj = adj + sp.eye(adj.shape[0])
+    row_sum = np.array(adj.sum(1))
+    row_sum = (row_sum == 0) * 1 + row_sum
+    d_inv_sqrt = np.power(row_sum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return d_mat_inv_sqrt.dot(adj).dot(d_mat_inv_sqrt).tocoo()
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+
+def load_citation(dataset_str="cora", device=0):
+    """
+    Load Citation Networks Datasets.
+    """
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open("/home/ljw/tyh/CPF/data/planetoid/ind.{}.{}".format(dataset_str.lower(), names[i]), 'rb') as f:
+            if sys.version_info > (3, 0):
+                objects.append(pkl.load(f, encoding='latin1'))
+            else:
+                objects.append(pkl.load(f))
+
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file(
+        "/home/ljw/tyh/CPF/data/planetoid/ind.{}.test.index".format(dataset_str))
+    test_idx_range = np.sort(test_idx_reorder)
+
+    if dataset_str == 'citeseer':
+        # Fix citeseer dataset (there are some isolated nodes in the graph)
+        # Find isolated nodes, add them as zero-vecs into the right position
+        test_idx_range_full = range(
+            min(test_idx_reorder),
+            max(test_idx_reorder) + 1)
+        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
+        tx_extended[test_idx_range - min(test_idx_range), :] = tx
+        tx = tx_extended
+        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
+        ty_extended[test_idx_range - min(test_idx_range), :] = ty
+        ty = ty_extended
+
+    features = sp.vstack((allx, tx)).tolil()
+    features[test_idx_reorder, :] = features[test_idx_range, :]
+    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    labels = np.vstack((ally, ty))
+    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    idx_test = test_idx_range.tolist()
+    idx_train = list(range(len(y)))
+    idx_val = list(range(len(y), len(y) + 500))
+
+    adj = normalize(adj + sp.eye(adj.shape[0]))
+    # features = normalize(features)
+    # porting to pytorch
+    features = torch.FloatTensor(np.array(features.todense())).float().to(device)
+    labels_one_hot = torch.FloatTensor(labels)
+    labels = labels.argmax(axis=1)
+    labels = torch.LongTensor(labels).to(device)
+    # adj = sparse_mx_to_torch_sparse_tensor(adj).float()
+    idx_train = torch.LongTensor(idx_train).to(device)
+    idx_val = torch.LongTensor(idx_val).to(device)
+    idx_test = torch.LongTensor(idx_test).to(device)
+    #adj = sys_normalized_adjacency(adj)
+    adj_sp = adj.tocoo()
+    adj = torch.FloatTensor(np.array(adj.todense()))
+    adj = sys_normalized_adjacency(adj)
+    adj = sparse_mx_to_torch_sparse_tensor(adj).to(device)
+    features = features.to(device)
+    labels = labels.to(device)
+    labels_one_hot = labels_one_hot.to(device)
+
+    return adj, adj_sp, features, labels, labels_one_hot, idx_train, idx_val, idx_test
